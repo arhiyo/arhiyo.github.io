732c8cb8afde337179c76d26671b12faa977ffb6
oleksandr arkhipov <oleksandr.arkhipov@yomobile.com>
2024-02-21 23:34:45 +0200

    Initial commit

--- js/receiver.js
+++ js/receiver.js
@@ -16,9 +16,30 @@ const authorizationKey = 'Authorization'
 const playbackConfig = new cast.framework.PlaybackConfig();
 const headers = {};
 
+const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
+const LOG_TAG = 'MyAPP.LOG';
+
+context.addEventListener(cast.framework.system.EventType.READY, () => {
+  if (!castDebugLogger.debugOverlayElement_) {
+      castDebugLogger.setEnabled(true);
+  }
+});
+
+castDebugLogger.loggerLevelByEvents = {
+  'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,
+  'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG
+}
+
+// Set verbosity level for custom tags.
+castDebugLogger.loggerLevelByTags = {
+    [LOG_TAG]: cast.framework.LoggerLevel.DEBUG,
+};
+
 playerManager.setMessageInterceptor(
   cast.framework.messages.MessageType.LOAD,
   request => {
+    castDebugLogger.info(LOG_TAG, 'Intercepting LOAD request');
+
     headers[mediaTokenKey] = request.media.customData['mediaTokenKey'];
     headers[authorizationKey] = request.media.customData['authorizationKey'];
 
@@ -29,6 +50,7 @@ playerManager.setMessageInterceptor(
     };
 
     playerManager.setPlaybackConfig(playbackConfig);
+    const data = request["media"]['contentId'];
 
         console.log("listen", data);
     request.media.hlsSegmentFormat = cast.framework.messages.HlsSegmentFormat.TS;
