2a9e88192e263dd3e7320928bdf6978d4b167657
arhiyo <159797364+arhiyo@users.noreply.github.com>
2025-04-11 21:11:58 +0300

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -3,67 +3,41 @@ const playerManager = context.getPlayerManager();
 
 const StreamType = {
   DASH: 'application/dash+xml',
-  HLS: 'application/x-mpegurl',
-};
+  HLS: 'application/x-mpegurl'
+}
+const TEST_STREAM_TYPE = StreamType.DASH
 
-const STREAM_TYPE = StreamType.HLS; 
-
-const mediaTokenKey = 'MEDIA-TOKEN';
-const authorizationKey = 'Authorization';
-const headers = {};
-
-const playbackConfig = new cast.framework.PlaybackConfig();
-const castReceiverOptions = new cast.framework.CastReceiverOptions();
-
+let castReceiverOptions = new cast.framework.CastReceiverOptions();
 castReceiverOptions.useShakaForHls = true;
-castReceiverOptions.shakaVersion = '4.7.12';
+castReceiverOptions.shakaVersion = '4.2.2';
 
-window.onerror = function (message, source, lineno, colno, error) {
-  logError(`[Global Error] ${message} at ${source}:${lineno}:${colno}`, error);
-};
-
-window.onunhandledrejection = function (event) {
-  logError('[Unhandled Promise Rejection]', event.reason);
-};
-playerManager.addEventListener(cast.framework.events.EventType.ERROR, (event) => {
-  logError('[PlayerManager Error]', event);
-});
+const mediaTokenKey = 'MEDIA-TOKEN'
+const authorizationKey = 'Authorization'
+const playbackConfig = new cast.framework.PlaybackConfig();
+const headers = {};
 
 playerManager.setMessageInterceptor(
   cast.framework.messages.MessageType.LOAD,
-  (request) => {
-    
-    const token = request['customData']['mediaTokenKey'];
-    const auth = request['customData']['authorizationKey'];
-    
-    headers[mediaTokenKey] = token;
-    headers[authorizationKey] = auth;
-    playbackConfig.manifestRequestHandler = requestInfo => {
-        requestInfo.headers = headers;
-    };
-    playbackConfig.shakaConfiguration = {
-        networking: {
-          fetch: {
-            headers: {
-              [mediaTokenKey]: token,
-              [authorizationKey]: auth,
-            },
-          },
-        },
-    };
-    logError('request', request);
+  request => {
     playerManager.setPlaybackConfig(playbackConfig);
-    log('[Receiver] PlaybackConfig set.');
+    headers[mediaTokenKey] = request.customData.mediaTokenKey;
+    headers[authorizationKey] = request.customData.authorizationKey;
 
-    request.media.contentType = STREAM_TYPE;
+    playbackConfig.manifestRequestHandler = requestInfo => {
+      requestInfo.headers = headers
+    };
+
     request.media.hlsSegmentFormat = cast.framework.messages.HlsSegmentFormat.TS;
     request.media.hlsVideoSegmentFormat = cast.framework.messages.HlsVideoSegmentFormat.TS;
-    log(`[Receiver] Content type set to ${request.media.contentType}`);
-    return Promise.resolve(request);
-  }
-);
+    request.media.contentType = StreamType.HLS;
 
-context.start(castReceiverOptions);
+    return new Promise((resolve, reject) => {
+        resolve(request);
+      });
+  });
+
+
+context.start(castReceiverOptions)
 
 context.addEventListener(cast.framework.system.EventType.ERROR, (e) => {
   logError('[Cast Receiver] Error:', e);
