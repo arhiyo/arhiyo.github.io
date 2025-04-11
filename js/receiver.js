a0514c43c04547f02d63b0cf53ab1b54ff4184c7
arhiyo <159797364+arhiyo@users.noreply.github.com>
2025-04-11 18:08:28 +0300

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -62,8 +62,8 @@ playerManager.setMessageInterceptor(
       log('[Receiver] PlaybackConfig set.');
 
       request.media.contentType = TEST_STREAM_TYPE;
-      // request.media.hlsSegmentFormat = cast.framework.messages.HlsSegmentFormat.TS;
-      // request.media.hlsVideoSegmentFormat = cast.framework.messages.HlsVideoSegmentFormat.FMP4;
+      request.media.hlsSegmentFormat = cast.framework.messages.HlsSegmentFormat.TS;
+      request.media.hlsVideoSegmentFormat = cast.framework.messages.HlsVideoSegmentFormat.FMP4;
 
       log(`[Receiver] Content type set to ${request.media.contentType}`);
 
