7eb1564367dbaf2d8c514c066c4f46d0259907df
arhiyo <159797364+arhiyo@users.noreply.github.com>
2025-04-14 10:55:40 +0300

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -3,58 +3,50 @@ const playerManager = context.getPlayerManager();
 
 const StreamType = {
   DASH: 'application/dash+xml',
-  HLS: 'application/x-mpegurl'
-}
-const TEST_STREAM_TYPE = StreamType.DASH
+  HLS: 'application/x-mpegurl',
+};
 
-let castReceiverOptions = new cast.framework.CastReceiverOptions();
-castReceiverOptions.useShakaForHls = true;
-castReceiverOptions.shakaVersion = '4.2.2';
+const STREAM_TYPE = StreamType.HLS; 
 
-const mediaTokenKey = 'MEDIA-TOKEN'
-const authorizationKey = 'Authorization'
-const playbackConfig = new cast.framework.PlaybackConfig();
+const mediaTokenKey = 'MEDIA-TOKEN';
+const authorizationKey = 'Authorization';
 const headers = {};
+
+const playbackConfig = new cast.framework.PlaybackConfig();
+const castReceiverOptions = new cast.framework.CastReceiverOptions();
+
+castReceiverOptions.useShakaForHls = true;
+castReceiverOptions.shakaVersion = '4.7.12';
 
 playerManager.setMessageInterceptor(
   cast.framework.messages.MessageType.LOAD,
-  request => {
-    playerManager.setPlaybackConfig(playbackConfig);
-    headers[mediaTokenKey] = request.customData.mediaTokenKey;
-    headers[authorizationKey] = request.customData.authorizationKey;
-
+  (request) => {
+    
+    const token = request['customData']['mediaTokenKey'];
+    const auth = request['customData']['authorizationKey'];
+    
+    headers[mediaTokenKey] = token;
+    headers[authorizationKey] = auth;
     playbackConfig.manifestRequestHandler = requestInfo => {
-      requestInfo.headers = headers
+        requestInfo.headers = headers;
     };
+    playbackConfig.shakaConfiguration = {
+        networking: {
+          fetch: {
+            headers: {
+              [mediaTokenKey]: token,
+              [authorizationKey]: auth,
+            },
+          },
+        },
+    };
+    playerManager.setPlaybackConfig(playbackConfig);
 
+    request.media.contentType = STREAM_TYPE;
     request.media.hlsSegmentFormat = cast.framework.messages.HlsSegmentFormat.TS;
     request.media.hlsVideoSegmentFormat = cast.framework.messages.HlsVideoSegmentFormat.TS;
-    request.media.contentType = StreamType.HLS;
-
-    return new Promise((resolve, reject) => {
-        resolve(request);
-      });
-  });
-
-
-context.start(castReceiverOptions)
-
-context.addEventListener(cast.framework.system.EventType.ERROR, (e) => {
-  logError('[Cast Receiver] Error:', e);
-});
-
-function log(message) {
-  console.log(message);
-  const logDiv = document.getElementById('log');
-  if (logDiv) {
-    const timestamp = new Date().toISOString().split('T')[1].split('.')[0];
-    logDiv.innerText += `[${timestamp}] ${message}\n`;
-    logDiv.scrollTop = logDiv.scrollHeight; 
+    return Promise.resolve(request);
   }
-}
+);
 
-function logError(message, error) {
-  console.error(message, error);
-  const details = typeof error === 'object' ? JSON.stringify(error, null, 2) : error;
-  log(`${message} ${details}`);
-}
+context.start(castReceiverOptions);
