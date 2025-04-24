feb43d49a9694c16b720cb353090e6221b31c86d
arhiyo <159797364+arhiyo@users.noreply.github.com>
2025-04-24 19:04:55 +0300

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -17,7 +17,7 @@ const castReceiverOptions = new cast.framework.CastReceiverOptions();
 
 // Set Shaka player options
 castReceiverOptions.useShakaForHls = true;
-castReceiverOptions.shakaVersion = '4.6.12';
+castReceiverOptions.shakaVersion = '4.6.13';
 
 // Global error handler
 window.onerror = function (message, source, lineno, colno, error) {
